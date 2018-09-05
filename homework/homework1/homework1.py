0123b96347481013ff01cf86d817b4c599f7424c
Juan Castillo <juancstl@protonmail.com>
2018-09-04 21:04:07 -0700

    Commit HW1 template

---
+++ homework/homework1/homework1.py
@@ -0,0 +1,58 @@
+# -----------------------------------------------------------------------------
+# Name:        homework1
+# Purpose:     Practice writing Python functions
+#
+# Author:
+# -----------------------------------------------------------------------------
+"""
+Implement some helper functions that will be useful in subsequent assignments
+"""
+
+
+def manhattan_distance(point1, point2):
+    """
+    Compute the Manhattan distance between two points.
+    :param point1 (tuple) representing the coordinates of a point in a plane
+    :param point2 (tuple) representing the coordinates of a point in a plane
+    :return: (integer)  The Manhattan distance between the two points
+    """
+    # Enter your code here and remove the pass statement below
+    pass
+
+
+def min_distance(point1, other_points):
+    """
+    Find the minimum Manhattan distance from point1 to the other points
+    :param point1 (tuple) representing the coordinates of a point in a plane
+    :param other_points (set of tuples) representing several points in a plane
+    :return: (integer) maximum Manhattan distance from point1 to the other
+    points
+
+    """
+    # Enter your code here and remove the pass statement below
+    pass
+
+
+def farthest_point(point1, other_points):
+    """
+    Find the coordinates of the farthest point to point1
+    :param point1 (tuple) representing the coordinates of a point in a plane
+    :param other_points(set of tuples) representing several points in a
+    plane
+    :return: (tuple) the coordinates of the farthest point to point1
+
+    """
+    # Enter your code here and remove the pass statement below
+    pass
+
+
+def farthest_distance(points):
+    """
+    Find the maximum Manhattan distance between all the points given
+    :param points(list of tuples) representing several points in a
+    plane
+    :return: (integer) maximum Manhattan distance between any two points
+    in the list given.
+    """
+    # Enter your code here and remove the pass statement below
+    pass
